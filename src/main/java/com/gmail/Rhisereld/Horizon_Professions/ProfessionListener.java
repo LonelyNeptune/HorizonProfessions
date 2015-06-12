34e151f3a1b05297e4eebf7ea2d40b350d59c32a
Rhisereld <Rhisereld@gmail.com>
2015-06-12 19:02:35 +1000

    Added heal listener, which activates when a player right-clicks another player with a certain item in their hand. If the item is in the configuration file, the recipient will be healed by an amount specified in the configuration file for their tier. Experience gained by doing this is also specified in the configuration file.

--- src/main/java/com/gmail/Rhisereld/Horizon_Professions/ProfessionListener.java
+++ src/main/java/com/gmail/Rhisereld/Horizon_Professions/ProfessionListener.java
@@ -2,6 +2,8 @@ package com.gmail.Rhisereld.Horizon_Professions;
 
 import java.util.Set;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,8 +11,10 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 /*
@@ -81,10 +85,126 @@ public class ProfessionListener implements Listener
     			exp = main.config.getConfig().getInt("slaying." + monster + ".exp");
     			
     			//Check that the player doesn't have practice fatigue.
-    			if (main.getPracticeFatigue(player, profession ) > 0)
-    				return;
+    			if (main.getPracticeFatigue(player, profession) > 0)
+    				continue;
     			
     			main.gainExperience(player, profession, exp);
     		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerInteract(PlayerInteractEntityEvent event)
+	{
+		final Player player = event.getPlayer();
+		final Player recipient;
+		String profession;
+		int playerTier = -1;
+		
+		player.sendMessage("PlayerInteractEntityEvent");
+		
+    	player.sendMessage(Double.toString(player.getVelocity().length()));
+		
+		//Check that the player right-clicked on another player.
+		if (!(event.getRightClicked() instanceof Player))
+			return;
+		
+		player.sendMessage("Right-clicked on player");
+		
+		recipient = (Player) event.getRightClicked();
+		
+		//See if options are specified in the configuration file.
+    	Set <String> items = main.config.getConfig().getConfigurationSection("healing.").getKeys(false);
+
+    	for (final String item: items)
+    	{
+    		player.sendMessage("In config: " + item);
+        	
+    		//Check if the item in hand fits any of the items specified in the configuration file.
+    		if (player.getItemInHand().getType().toString().equalsIgnoreCase(item))
+    		{
+    			profession = main.config.getConfig().getString("healing." + item + ".profession");
+    			
+        		player.sendMessage("Item match");
+    			
+    			//Check if the amount to heal is in the config
+    	    	Set <String> tiers = main.config.getConfig().getConfigurationSection("healing." + item + ".tier").getKeys(false);
+    	    	
+    	    	for (String tier: tiers)
+    	    		if (main.TIERS[main.getTier(player, profession)].equalsIgnoreCase(tier))
+    	    		{
+    	    			playerTier = main.config.getConfig().getInt("healing." + item + ".tier." + tier);
+    	    			player.sendMessage(Integer.toString(playerTier));
+    	    		}
+
+    	    	//If it isn't in the config just stop.
+    	    	if (playerTier == -1)
+    	    	{
+    	    		player.sendMessage("playerTier is null");
+    	    		return;
+    	    	}
+    	    	
+    			//Check that the recipient has missing health.
+    			if (recipient.getHealth() >= recipient.getMaxHealth())
+    			{
+    				player.sendMessage(recipient.getName() + "does not need bandaging!");
+    				return;
+    			}  
+    			
+    			player.sendMessage("Bandaging...");
+    			
+    			//Schedule the task in one second.
+    			makeDelayedTask(player, recipient, playerTier, item, profession, player.getLocation(),  recipient.getLocation());
+    		}
+    	}
+	}
+	
+	void makeDelayedTask(final Player player, final Player recipient, final int playerTier, final String item, 
+			final String profession, final Location playerLoc, final Location recipientLoc)
+	{
+		//After a second, perform the action.
+		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() 
+		{
+			public void run() 
+			{
+				double addHp;  
+				int exp;
+				
+				//Check that the player is still in roughly the same location
+				if (Math.abs(player.getLocation().getX() - playerLoc.getX()) > 1
+						|| Math.abs(player.getLocation().getY() - playerLoc.getY()) > 1
+						|| Math.abs(player.getLocation().getZ() - playerLoc.getZ()) > 1)
+				{
+					player.sendMessage("You cannot move while bandaging!");
+					return;
+				}
+				
+				//Check that the recipient is still in roughly the same location
+				if (Math.abs(recipient.getLocation().getX() - recipientLoc.getX()) > 1
+						|| Math.abs(recipient.getLocation().getY() - recipientLoc.getY()) > 1
+						|| Math.abs(recipient.getLocation().getZ() - recipientLoc.getZ()) > 1)
+				{
+					player.sendMessage("You cannot bandage your patient while they are moving!");
+					return;
+				}
+				
+				//Remove item from player's inventory.
+	    		player.getInventory().removeItem(new ItemStack(Material.getMaterial(item), 1));
+	    		player.updateInventory();
+	    			
+	    		//Heal the other player.
+	    		addHp = main.config.getConfig().getDouble("healing." + item + ".tier" + playerTier);
+	    		recipient.setHealth(recipient.getHealth() + addHp);
+
+	    		//Award experience.
+	    		exp = main.config.getConfig().getInt("healing." + item + ".exp");
+	    		
+	    		if (main.getPracticeFatigue(player, profession) <= 0)
+	    			main.gainExperience(player,  profession, exp);
+	    			
+	    		//Notify both parties.
+	    		player.sendMessage("You bandaged " + recipient.getName() + "'s wounds.");
+	    		recipient.sendMessage(player.getName() + " bandaged your wounds.");
+			  }
+			}, 20);
 	}
 }
